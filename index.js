6ce07d620722189ef7ab1732e7ca2fe11dd86bc9
Abdrahmanalkautsar <58649363+Abdrahmanalkautsar@users.noreply.github.com>
2025-02-01 16:25:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,415 @@
+/**
+ * Entry point of application, where App is rendered within the div with the id of "app" 
+ */
+
+
+//import App from "./App";
+const siteProps = {
+  name: "Abd Rahman",
+  title: "Student",
+  email: "abdr5259@gmail.com",
+  gitHub: "Abdrahmanalkautsar",
+  instagram: "abdrahmanalkautsar400",
+  linkedIn: "ABD RAHMAN ALKAUTSAR",
+  About: "This template from Microsoft Learn",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+//Header
+/**
+ * Header component
+ *
+ * Top navigation bar for your site. Set to remain visible as the
+ * user scrolls so that they can constantly reach any part of your page.
+ */
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        display: "flex",
+        justifyContent: "center",
+        gap: "2rem",
+        background: "rgba(255,255,255,0.75)",
+        padding: "1rem",
+        top: 0,
+        width: "100%",
+        zIndex: 10,
+      }}
+    >
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#footer">Contact</a>
+    </div>
+  );
+};
+//About
+/**
+ * About component
+ *
+ * Space for you to describe more about yourself.
+ */
+
+/**
+ * About background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "./images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsList = [
+  "Web design",
+  "User experience",
+  "Inclusive design",
+  "Focus group testing",
+  "Mobile user interfaces",
+  "Graphic design",
+];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+
+const About = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>About Myself</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 2,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsList.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+//footer
+/**
+ * Footer component
+ *
+ * Displays avenues to contact you.
+ * Contact information is passed in from the App component that
+ * renders the Footer.
+ *
+ * If a social value has an empty string it will not be displayed.
+ */
+
+import devDotToIcon from "./images/devdotto.svg";
+import envelopeIcon from "./images/envelope.svg";
+import gitHubIcon from "./images/github.svg";
+import instagramIcon from "./images/instagram.svg";
+import linkedInIcon from "./images/linkedin.svg";
+import mediumIcon from "./images/medium.svg";
+import twitterIcon from "./images/twitter.svg";
+import youTubeIcon from "./images/youtube.svg";
+
+/**
+ * 💡 Learning resources
+ *
+ *  HTML hyperlinks: https://www.w3schools.com/html/html_links.asp
+ *  Opening links in new tabs: https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
+ */
+
+const Footer = (props) => {
+  const {
+    devDotTo,
+    email,
+    gitHub,
+    instagram,
+    linkedIn,
+    medium,
+    name,
+    primaryColor,
+    twitter,
+    youTube,
+  } = props;
+
+  return (
+    <div
+      id="footer"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2.5rem",
+        padding: "5rem 0 3rem",
+        backgroundColor: primaryColor,
+        width: "100vw"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        {email && (
+          <a href={`mailto:${email}`}>
+            <img src={envelopeIcon} alt="email" className="socialIcon" />
+          </a>
+        )}
+        {devDotTo && (
+          <a href={`https://dev.to/${devDotTo}`} target="_blank" rel="noopener noreferrer">
+            <img src={devDotToIcon} alt="Dev.to" className="socialIcon" />
+          </a>
+        )}
+        {gitHub && (
+          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+            <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
+          </a>
+        )}
+        {instagram && (
+          <a
+            href={`https://www.instagram.com/${instagram}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={instagramIcon} alt="Instagram" className="socialIcon" />
+          </a>
+        )}
+        {linkedIn && (
+          <a
+            href={`https://www.linkedin.com/in/${linkedIn}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInIcon} alt="LinkedIn" className="socialIcon" />
+          </a>
+        )}
+        {medium && (
+          <a href={`https://medium.com/@${medium}`} target="_blank" rel="noopener noreferrer">
+            <img src={mediumIcon} alt="Medium" className="socialIcon" />
+          </a>
+        )}
+        {twitter && (
+          <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener noreferrer">
+            <img src={twitterIcon} alt="Twitter" className="socialIcon" />
+          </a>
+        )}
+        {youTube && (
+          <a
+            href={`https://www.youtube.com/c/${youTube}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={youTubeIcon} alt="YouTube" className="socialIcon" />
+          </a>
+        )}
+      </div>
+      <p className="small" style={{ marginTop: 0, color: "white" }}>
+        Created by {name}
+      </p>
+    </div>
+  );
+};
+
+Footer.defaultProps = {
+  name: "",
+};
+
+Footer.propTypes = {
+  devDotTo: PropTypes.string,
+  email: PropTypes.string,
+  gitHub: PropTypes.string,
+  instagram: PropTypes.string,
+  linkedIn: PropTypes.string,
+  medium: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  primaryColor: PropTypes.string,
+  twitter: PropTypes.string,
+  youTube: PropTypes.string,
+
+};
+//Home
+/**
+ * Home component
+ *
+ * The section at the top of the page to display image of your
+ * choice, name and title that describes your career focus.
+ */
+import arrowSvg from "./images/down-arrow.svg";
+import PropTypes from "prop-types";
+
+/**
+ * Home background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "./images/woman-with-tablet.jpg";
+
+const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+
+const Home = ({ name, title }) => {
+  return (
+    <section id="home" className="min-height">
+      <img className="background" src={image} alt="" />
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+        <h1>{name}</h1>
+        <h2>{title}</h2>
+      </div>
+      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
+        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      </div>
+    </section>
+  );
+};
+
+Home.defaultProps = {
+  name: "",
+  title: "",
+};
+
+Home.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+//Portofoli
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+ 
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "./images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    description:
+      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+  },
+  {
+    title: "Web Development for Beginners",
+    description:
+      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    url: "https://github.com/microsoft/web-dev-for-beginners",
+  },
+  {
+    title: "My Resume Site",
+    description:
+      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+  },
+  {
+    title: "GitHub Codespaces and github.dev",
+    description:
+      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
